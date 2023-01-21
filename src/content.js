91157e3e8eb9096ee11440d6d810ef0e5c00e4ae
BlankCanvasTM <BlankCanvasTM@users.noreply.github.com>
2023-01-21 15:45:23 +0000

    90% Navbar and started main content

---
+++ src/content.js
@@ -0,0 +1,18 @@
+const Content = () => {
+    return ( 
+        <div className="content">
+
+            <ul>
+
+                <li><a className="specialText" href="">About</a></li>
+                <li><a href="">Jobs</a></li>
+                <li><a href="">Apprenticeships</a></li>
+
+            </ul>
+
+
+        </div>
+     );
+}
+ 
+export default Content;
---
+++ src/navbar.js
@@ -0,0 +1,35 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            
+            <ul className="right-side">
+
+                <li><a href="">Push</a></li>
+                <li><a href="">Note</a></li>
+                <li><a href="">Link</a></li>
+                <li><a href="">Shop</a></li>
+                <li><a href="">Pack</a></li>
+                <li><a href="">Help</a></li>
+                
+                <li><a className="specialText" href="/">More +</a></li>
+
+            </ul>
+
+            <ul className="left-side">
+
+                <li><a href="">Try Live for Free</a></li>
+                <li><a href="">Log in or register</a></li>
+
+            </ul>
+            
+
+            
+        
+        </nav>
+        
+
+
+     );
+}
+ 
+export default Navbar;
